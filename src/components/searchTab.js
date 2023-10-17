2d22bd07b35116af0b63d7f94103914aaa9bdb28
mugi7794 <mugi7794@naver.com>
2023-10-17 22:56:41 +0900

    feat/RecentList

--- src/components/searchTab.js
+++ src/components/searchTab.js
@@ -4,21 +4,60 @@ import { useState } from "react";
 import SearchApi from "../apis/search.api";
 
 const SearchTap = () => {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState([]);
+  const [RecentList, setRecentList] = useState([]);
+  const [keyWord, setKeyWord] = useState("");
+
   const onTypeChange = async (e) => {
+    e.preventDefault();
+    setKeyWord(e.target.value);
     try {
       const data = await SearchApi.getSearch(e.target.value);
       console.log(data);
       setSearch(data);
     } catch (err) {
-      setSearch(err.response.data);
+      console.error(err.response.data);
+      setSearch([err.response.data]);
+    }
+  };
+
+  const onClickSearch = () => {
+    if (RecentList.length >= 5) {
+      RecentList.pop();
+      setRecentList([keyWord, ...RecentList]);
+    } else {
+      setRecentList([keyWord, ...RecentList]);
     }
   };
   return (
     <>
       <Wrap>
-        <input type="text" onChange={onTypeChange}></input>
-        {search}
+        <InputContainer>
+          <input
+            type="text"
+            onChange={onTypeChange}
+            placeholder="검색어를 입력해주세요"
+          ></input>
+          <button onClick={() => onClickSearch()}>검색버튼</button>
+        </InputContainer>
+        <RecentContainer>
+          {RecentList.length >= 1 && (
+            <>
+              <div>최근검색어</div>
+              <ul>
+                {RecentList.map((list, index) => (
+                  <li key={index}>{list}</li>
+                ))}
+              </ul>
+            </>
+          )}
+        </RecentContainer>
+        <div>관련검색어</div>
+        <ul>
+          {search.map((data, index) => (
+            <li key={index}>{data}</li>
+          ))}
+        </ul>
       </Wrap>
     </>
   );
@@ -30,4 +69,9 @@ const Wrap = styled.div`
   width: 100%;
   height: 100%;
   ${flexAlignCenter}
+  flex-direction: column;
 `;
+
+const InputContainer = styled.div``;
+
+const RecentContainer = styled.div``;
